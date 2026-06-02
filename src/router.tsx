8d2e980b25e77cf3ad08eb6aff98089b7ea6c171
Arjun Sharma <arjunsharma3505@gmail.com>
2026-06-02 12:41:47 +0530

    first commit

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-
+//jskc
 export const getRouter = () => {
   const queryClient = new QueryClient();
 
